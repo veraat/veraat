b70ee95c46d014cb4175495e2462b1d583cd0d2c
veraat <77840812+veraat@users.noreply.github.com>
2021-01-22 18:30:31 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,36 @@
+alert ("IN MINUS PUT THE BIGGER NUMBER FIRST AND IN THE DIVISION PUT DIVIDEND FIRST")
+
+function add() {
+    var result=
+        Number(document.getElementById("firstno").value)+
+        Number(document.getElementById("secondno").value);
+    window.alert(result);
+    }
+
+    function SUB() {
+        var result=
+            Number(document.getElementById("firstno").value)-
+            Number(document.getElementById("secondno").value);
+        window.alert(result);
+        }
+
+        function MUL() {
+            var result=
+                Number(document.getElementById("firstno").value)*
+                Number(document.getElementById("secondno").value);
+            window.alert(result);
+            }
+
+            function DIV() {
+                var result=
+                    Number(document.getElementById("firstno").value)/
+                    Number(document.getElementById("secondno").value);
+                window.alert(result);
+                }
+    
+
+            
+            
+            
+
+    
